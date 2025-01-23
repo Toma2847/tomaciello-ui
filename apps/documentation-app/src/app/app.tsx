23c1d69950eb7fb363f5d489d2f9d6a1b104a80f
Toma1304 <francesco.tomaciello@gmail.com>
2025-01-23 16:28:53 +0100

    update form e update header

--- apps/documentation-app/src/app/app.tsx
+++ apps/documentation-app/src/app/app.tsx
@@ -4,12 +4,17 @@ import { Button } from '@tomaciello-ui/button';
 
 import NxWelcome from './nx-welcome';
 import { Footer } from '@tomaciello-ui/footer';
+import { Form } from '@tomaciello-ui/form';
+import { Header } from '@tomaciello-ui/header';
+
 
 export function App() {
   return (
     <div>
       <Button/>
       <Footer/>
+      <Form/>
+      <Header/>
     </div>
   );
 }
