860577d2619332f0948bedb6ba622d0bc48e0e4d
Toma1304 <francesco.tomaciello@gmail.com>
2025-01-23 15:15:53 +0100

    update footer

--- apps/documentation-app/src/app/app.tsx
+++ apps/documentation-app/src/app/app.tsx
@@ -3,11 +3,13 @@ import styles from './app.module.css';
 import { Button } from '@tomaciello-ui/button';
 
 import NxWelcome from './nx-welcome';
+import { Footer } from '@tomaciello-ui/footer';
 
 export function App() {
   return (
     <div>
       <Button/>
+      <Footer/>
     </div>
   );
 }
