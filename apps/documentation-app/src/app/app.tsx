6128d8d98eba53faf15e0241bfb9550926489aa9
Toma1304 <francesco.tomaciello@gmail.com>
2025-01-23 14:51:22 +0100

    update button

--- apps/documentation-app/src/app/app.tsx
+++ apps/documentation-app/src/app/app.tsx
@@ -1,12 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import { Button } from '@tomaciello-ui/button';
 
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="documentation-app" />
+      <Button/>
     </div>
   );
 }
